39ea0b4a70224c92442d4d2aa259f3fdea4e2565
Masayur <muaji.risky@qmuaji.com>
2021-09-20 19:57:19 +0700

    Update laporanTransaksiAkta_xls.php

--- xls/laporanTransaksiAkta_xls.php
+++ xls/laporanTransaksiAkta_xls.php
@@ -33,28 +33,38 @@ $objPHPExcel->getProperties()->setCreator("Risky Muaji")
  
 // Add some data
 $objPHPExcel->setActiveSheetIndex(0)
-       ->setCellValue('A1', 'Tanggal')
-       ->setCellValue('B1', 'Kode Transaksi')
-       ->setCellValue('C1', 'Nama Penghadap')
-       ->setCellValue('D1', 'Jenis Transaksi')
-       ->setCellValue('E1', 'Nama Akta')
-       ->setCellValue('F1', 'Harga')
-       ->setCellValue('G1', 'Sisa Tagihan')
-       ->setCellValue('H1', 'Keterangan');
+       ->setCellValue('A1', 'Kode Transaksi')
+       ->setCellValue('B1', 'Tanggal Transaksi')
+       ->setCellValue('C1', 'No Akta')
+       ->setCellValue('D1', 'Tanggal Akta')
+       ->setCellValue('E1', 'No. SK')
+       ->setCellValue('F1', 'Jenis Akta')
+       ->setCellValue('G1', 'Nama Akta')
+       ->setCellValue('H1', 'Deskripsi')
+       ->setCellValue('I1', 'Harga')
+       ->setCellValue('J1', 'Sudah Bayar')
+       ->setCellValue('K1', 'Sisa Tagihan')
+       ->setCellValue('L1', 'Keterangan')
+       ->setCellValue('M1', 'Nama Penghadap');
  
 $baris = 2;
 $no = 0;			
 while($row=mysql_fetch_array($hasil)){
   $no = $no +1;
   $objPHPExcel->setActiveSheetIndex(0)
-       ->setCellValue("A$baris", $row['TglTransaksi'])
-       ->setCellValue("B$baris", $row['KdTransaksi'])
-       ->setCellValue("C$baris", $row['NamaLengkap'])
-       ->setCellValue("D$baris", $row['JenisAkta'])
-       ->setCellValue("E$baris", $row['NamaAkta'])
-       ->setCellValue("F$baris", $row['Harga'])
-       ->setCellValue("G$baris", $row['SisaTagihan'])
-       ->setCellValue("H$baris", $row['Keterangan']);
+       ->setCellValue("A$baris", $row['KdTransaksi'])
+       ->setCellValue("B$baris", $row['TglTransaksi'])
+       ->setCellValue("C$baris", $row['NoAkta'])
+       ->setCellValue("D$baris", $row['TanggalAkta'])
+       ->setCellValue("E$baris", $row['NoSK'])
+       ->setCellValue("F$baris", $row['JenisAkta'])
+       ->setCellValue("G$baris", $row['NamaAkta'])
+       ->setCellValue("H$baris", $row['Deskripsi'])
+       ->setCellValue("I$baris", $row['Harga'])
+       ->setCellValue("J$baris", $row['SudahBayar'])
+       ->setCellValue("K$baris", $row['SisaTagihan'])
+       ->setCellValue("L$baris", $row['Keterangan'])
+       ->setCellValue("M$baris", $row['NamaLengkap']);
   $baris = $baris + 1;
 }
  
